9d120ddf0d4883b9ef7ec93eb9dff645668349df
luigicara <luigi.caravelli98@gmail.com>
2023-03-02 18:44:07 +0100

    restaurants migration complete

--- database/migrations/2023_03_02_154558_create_restaurants_table.php
+++ database/migrations/2023_03_02_154558_create_restaurants_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name', 64);
+            $table->text('description');
+            $table->string('place', 64);
+            $table->smallInteger('phone_number');
+            $table->string('img');
+            $table->time('opening_time');
+            $table->time('closing_time');
+            $table->decimal('shipping_cost', $precision = 4, $scale = 2);
+            $table->string('closing_day', 16);
+
             $table->timestamps();
         });
     }
