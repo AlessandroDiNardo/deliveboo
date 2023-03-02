c979734b03df25f747cfc3985a9c9aa2b0367c29
LucaVitale00 <luca16vitale2000@gmail.com>
2023-03-02 16:39:21 +0100

    hola mundo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,7 @@
         </h1>
 
         <h2>Hello World</h2>
+        <h3>Hola Mundo</h3>
 
         <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 10.x</p>
         <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
