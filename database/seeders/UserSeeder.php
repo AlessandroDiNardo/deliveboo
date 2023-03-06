f0104ae8a844451652d1e1d5f8fb200f766e8ed9
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-06 18:08:38 +0100

    change number fake user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,6 +15,6 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory() -> count(5) -> create();
+        User::factory() -> count(18) -> create();
     }
 }
