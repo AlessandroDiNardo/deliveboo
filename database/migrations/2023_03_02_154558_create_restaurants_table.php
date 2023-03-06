3c56c919ffe523c11e42a84cf8f4a01e5ae1b6ee
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-06 18:07:30 +0100

    change string lenght 'place'

--- database/migrations/2023_03_02_154558_create_restaurants_table.php
+++ database/migrations/2023_03_02_154558_create_restaurants_table.php
@@ -18,9 +18,10 @@ return new class extends Migration
 
             $table->string('name', 64);
             $table->text('description');
-            $table->string('place', 64);
+            $table->string('place', 128);
             $table->string('phone_number', 32);
             $table->string('img');
+            $table->string('vat');
             $table->time('opening_time');
             $table->time('closing_time');
             $table->decimal('shipping_cost', $precision = 4, $scale = 2);
