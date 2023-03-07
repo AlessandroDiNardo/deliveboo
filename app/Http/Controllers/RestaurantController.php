87b8d1e0397bb843a8369d7f471c096fb1a01636
luigicara <luigi.caravelli98@gmail.com>
2023-03-07 15:55:18 +0100

    created controller for Restaurant and added create and store functions

---
+++ app/Http/Controllers/RestaurantController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// imported Models
+use App\Models\Restaurant;
+use App\Models\Category;
+use App\Models\User;
+
+class RestaurantController extends Controller
+{
+    public function create(Request $request) {
+        $categories = Category::all();
+
+        return view('pages.restaurant.create', compact('categories'));
+    }
+
+    public function store(Request $request) {
+
+        
+        $data = $request -> all();
+    
+        // validate([
+        //     'name' => 'required|string|min:3',
+        //     'description' => 'required|string|min:3',
+        //     'place' => 'required|string|min:3',
+        //     'phone_number' => 'required|string|min:3',
+        //     'img'  => 'required|string|min:3',
+        //     'vat'  => 'required|string|min:3',
+        //     'opening_time'  => 'required|string|min:0',
+        //     'closing_time' => 'required|string|min:0',
+        //     'shipping_cost' => 'required|integer|min:0',
+        //     'closing_day' => 'required|string|min:3',
+        //     'category_id' => 'required|array'
+        // ]);
+        
+        $user = User::find($request -> user() -> id);
+
+        $restaurant = Restaurant::make($data);
+
+        $restaurant -> user() -> associate($user);
+
+        $restaurant -> save();
+
+        $categories = Category :: find($data['category_id']);
+
+        $restaurant -> categories() -> sync($categories);
+
+        return redirect() -> route('profile.edit');
+    }
+}
