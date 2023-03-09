d0f02c47200609cbdf14bee8f16b65537f0f9213
luigicara <luigi.caravelli98@gmail.com>
2023-03-09 16:27:27 +0100

    fixed and changed copy

--- resources/views/pages/product/update.blade.php
+++ resources/views/pages/product/update.blade.php
@@ -1,17 +1,12 @@
 @extends('layouts.app')
 
 @php
-    $visible = array('true' => 'Disponibile', 'false' => 'Non Disponibile',)
+    $visible = array( 1 => 'Disponibile', 0 => 'Non Disponibile',)
 @endphp
 
 @section('content')
-<h1>CREA UN NUOVO PIATTO</h1>
-<form method="POST" action="{{ route('product.update') }}" enctype="multipart/form-data">
-    @csrf
-
-    <label for="name">Nome</label>
-    <input type="text" name="name"> <h1>CREA UN NUOVO PIATTO</h1>
-<form method="POST" action="{{ route('product.store') }}" enctype="multipart/form-data">
+<h1>Modifica Piatto</h1>
+<form method="POST" action="{{ route('product.update', $product) }}" enctype="multipart/form-data">
     @csrf
 
     <label for="name">Nome</label>
@@ -32,40 +27,12 @@
 
     <label for="visible">Disponibilità</label>
     <select name="visible">
-    <option value="true" selected>Disponibile</option>
-    <option value="false">Non Disponibile</option>
+        @foreach ($visible as $boolean => $value)
+            <option value="{{ $boolean }}" {{ $product -> visible == $boolean ? 'selected' : '' }}>{{ $value }}</option>
+        @endforeach
     </select>
     <br>
 
-    <input type="submit" value="CREA UN NUOVO PIATTO">
-</form>
-    <br>
-
-    <label for="ingredients">Ingredienti</label>
-    <input type="text" name="ingredients">
-    <br>
-
-    <label for="price">Prezzo</label>
-    <input type="number" name="price">
-    <br>
-
-    <label for="img">Immagine</label>
-    <input type="file" name="img">
-    <br>
-
-    <label for="visible">Disponibilità</label>
-    <select name="visible">
-        @foreach ($visible as $key => $value)
-        {{-- <option value="{{ $day }}" {{ $restaurant -> closing_day == $day ? 'selected' : '' }}>{{ $giorno }}</option>
-        @endforeach --}}
-            
-       
-    <option value="{{ $key }}" selected>Disponibile</option>
-    <option value="false">Non Disponibile</option>
-    @endforeach
-    </select>
-    <br>
-
-    <input type="submit" value="CREA UN NUOVO PIATTO">
+    <input type="submit" value="Aggiorna Piatto">
 </form>
 @endsection
