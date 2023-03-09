919c9522effd5f09d8829431ef95a7cd196bff04
luigicara <luigi.caravelli98@gmail.com>
2023-03-09 12:44:27 +0100

    added showAll()

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,34 +15,41 @@ use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
+    public function showAll(Request $request) {
+
+        $restaurant = Restaurant::where('user_id', $request -> user() -> id) -> first();
+
+        return view('pages.product.showAll', compact('restaurant'));
+    }
+
     public function create(Request $request) {
         $product = Product::all();
 
         return view('pages.product.create', compact('product'));
-}
+    }
 
-public function store(Request $request) {
+    public function store(Request $request) {
+            
+        $data = $request -> all();
+
+        // validate([
+
+
+        // ]);
+
+        // adding img
+        $img_path = Storage::put('productsImg', $data['img']);
+        $data['img'] = $img_path;
+
+
+        $restaurant = Restaurant::where('user_id', $request -> user() -> id) -> first();
+
+        $product = Product::make($data);
+
+        $product -> restaurant() -> associate($restaurant);
+        $product -> save();
+
+        return redirect() -> route('profile.edit');
         
-    $data = $request -> all();
-
-    // validate([
-
-
-    // ]);
-
-    // adding img
-    $img_path = Storage::put('uploads', $data['img']);
-    $data['img'] = $img_path;
-
-
-    $restaurant = Restaurant::where('user_id', $request -> user() -> id) -> first();
-
-    $product = Product::make($data);
-
-    $product -> restaurant() -> associate($restaurant);
-    $product -> save();
-
-    return redirect() -> route('profile.edit');
-    
-}
+    }
 }
