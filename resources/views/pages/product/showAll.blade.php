3bd776a2137b36ff71c6225790ba1de16d43af17
LucaVitale00 <luca16vitale2000@gmail.com>
2023-03-09 21:56:32 +0100

    fix card product

--- resources/views/pages/product/showAll.blade.php
+++ resources/views/pages/product/showAll.blade.php
@@ -2,12 +2,21 @@
 
 @section('content')
 
+<br>
+<div class="d-grid gap-2 d-md-flex d-flex justify-content-center">
+<a href="{{ route('product.create') }}" class="btn btn-success w-1 text-center">
+    Aggiungi Piatto
+</a>
+</div>
+<br>
+
 @foreach ( $restaurant -> products as $product)
 <div class="card" style="width: 18rem;">
     <div class="card" style="width: 18rem;">
         <img src="{{ asset('storage/' . $product -> img) }}" class="card-img-top" alt="productimg">
         <div class="card-body">
             <h3 class="card-title">{{ $product -> name }}</h3>
+            <h6>Prezzo: {{ $product -> price }}</h6>
             <h5>Ingredienti:</h5>
             <p class="card-text">{{ $product -> ingredients }}</p>
             <a href="{{ route('product.edit', $product) }}" class="btn btn-warning ">
@@ -20,9 +29,5 @@
         </div>
 </div>
 @endforeach 
-        
-<a href="{{ route('product.create') }}" class="btn btn-success w-20">
-    Aggiungi Piatto
-</a>
 
 @endsection
