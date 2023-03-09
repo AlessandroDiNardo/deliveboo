d818435d446b57e2d28e0bd1b66b20ef091f557e
luigicara <luigi.caravelli98@gmail.com>
2023-03-09 16:26:52 +0100

    edited href routes and added product.create button

--- resources/views/pages/product/showAll.blade.php
+++ resources/views/pages/product/showAll.blade.php
@@ -2,40 +2,27 @@
 
 @section('content')
 
-{{-- @foreach ( $restaurant -> products as $product)
-    <h3 class="mt-1 text-sm text-gray-600">
-        {{ $product -> name }}
-    </h3>
-
-    <p class="mt-1 text-sm text-gray-600">
-        {{ $product -> ingredients }}
-    </p>
-
-    <img src="{{ asset('storage/' . $product -> img) }}" alt="productimg">
-@endforeach
-    <a href="{{ route('product.create') }}" class="btn btn-success w-20">
-        Aggiungi Piatto
-    </a>  --}}
-    @foreach ( $restaurant -> products as $product)
+@foreach ( $restaurant -> products as $product)
+<div class="card" style="width: 18rem;">
     <div class="card" style="width: 18rem;">
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('storage/' . $product -> img) }}" class="card-img-top" alt="productimg">
-            <div class="card-body">
-              <h3 class="card-title">{{ $product -> name }}</h3>
-              <h5>Ingredienti:</h5>
-              <p class="card-text">{{ $product -> ingredients }}</p>
-              <a href="{{ route('restaurant.edit', $restaurant) }}" class="btn btn-warning ">
-                Modifica Piatto
-            </a>
-    
-            <a href="{{ route('restaurant.delete', $restaurant) }}" class="btn btn-danger ">
-                Elimina Piatto
-            </a>
-            </div>
-    </div>
-      @endforeach
-      <div>
-      
-        
+        <img src="{{ asset('storage/' . $product -> img) }}" class="card-img-top" alt="productimg">
+        <div class="card-body">
+            <h3 class="card-title">{{ $product -> name }}</h3>
+            <h5>Ingredienti:</h5>
+            <p class="card-text">{{ $product -> ingredients }}</p>
+            <a href="{{ route('product.edit', $product) }}" class="btn btn-warning ">
+            Modifica Piatto
+        </a>
+
+        <a href="{{ route('product.delete', $product) }}" class="btn btn-danger ">
+            Elimina Piatto
+        </a>
+        </div>
 </div>
+@endforeach 
+        
+<a href="{{ route('product.create') }}" class="btn btn-success w-20">
+    Aggiungi Piatto
+</a>
+
 @endsection
