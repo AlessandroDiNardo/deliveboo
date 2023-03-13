9e41e1cbcfe4d711984cc8d90bd91c18321cd224
luigicara <luigi.caravelli98@gmail.com>
2023-03-13 18:35:48 +0100

    added function to get Categories and filter restaurants

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -11,21 +11,34 @@ use App\Models\Product;
 
 class ApiController extends Controller
 {
-    public function getRestaurants() {
+    public function getRestaurants(Request $request) {
+        $categories = $request -> categories;
 
-        $restaurants = Restaurant :: with('categories', 'products')
-                    -> orderBy('created_at', 'desc')
-                    -> get();
-        $products = Product :: all();
+        $restaurants = Restaurant::all();
+
+        if ($categories) {
+            $restaurants = null;
+
+            foreach ($categories as $category) {
+                $restaurants = Restaurant::whereHas('categories', function ($q) use ($category) {
+                    $q->where('category_id', $category);
+                })->get();
+            }
+        }
+
+
+        return response() -> json([
+            'success' => true,
+            'response' => $restaurants,
+        ]);
+    }
+
+    public function getCategories() {
         $categories = Category :: all();
 
         return response() -> json([
             'success' => true,
-            'response' => [
-                'restaurants' => $restaurants,
-                'products' => $products,
-                'categories' => $categories,
-            ]
+            'response' => $categories,
         ]);
     }
 }
