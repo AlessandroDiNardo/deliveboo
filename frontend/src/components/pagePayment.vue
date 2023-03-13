2aa074c97e3ec20e5bddbe93e5fb84ae66deec47
furkanGit <furkancaliskan9801@gmail.com>
2023-03-13 16:51:11 +0100

    created App.vue and pagePayment.vue

---
+++ frontend/src/components/pagePayment.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+}
+</script>
+
+
+<template>
+
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/general.scss' as *;
+@use '../assets/scss/partials/variables.scss' as *;
+
+
+</style>
