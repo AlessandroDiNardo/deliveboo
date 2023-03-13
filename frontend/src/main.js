76c6642752711e68500f7ba8eb150ca89055d6cf
furkanGit <furkancaliskan9801@gmail.com>
2023-03-13 16:44:35 +0100

    install and  import boostrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+
+
+// import bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
 
 createApp(App).mount('#app')
