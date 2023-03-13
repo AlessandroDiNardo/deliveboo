202bc680388384d736fbdb7bf9d2fe2ead0b1439
Yari <narducciyarigmail.com>
2023-03-13 11:26:31 +0100

    metodo convalida

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,115 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", validate);
+
+function validate(e) {
+  e.preventDefault();
+
+  const firstNameField = document.getElementById("name");
+  const descriptionField = document.getElementById("input_description");
+  const placeField = document.getElementById("input_place");
+  const phoneField = document.getElementById("input_phone");
+  const vatField = document.getElementById("input_vat");
+  const shippingField =document.getElementById("input_shippingError");
+  const openingField = document.getElementById("input_opening");
+  const closingField = document.getElementById("input_closing");
+  const dayOffField = document.getElementById("day_off");
+  const imgField = document.getElementById("input_img");
+
+  let valid = true;
+
+  if (!firstNameField.value) {
+    const nameError = document.getElementById("nameError");
+    nameError.classList.add("visible");
+    firstNameField.classList.add("invalid");
+    nameError.setAttribute("aria-hidden", false);
+    nameError.setAttribute("aria-invalid", true);
+  }
+  if (!descriptionField.value) {
+    const inputDescriptionError = document.getElementById("inputDescriptionError");
+    inputDescriptionError.classList.add("visible");
+    descriptionField.classList.add("invalid");
+    inputDescriptionError.setAttribute("aria-hidden", false);
+    inputDescriptionError.setAttribute("aria-invalid", true);
+  }
+  
+  if (!phoneField.value) {
+    const inputPhoneError = document.getElementById("input_phoneError");
+    inputPhoneError.classList.add("visible");
+    phoneField.classList.add("invalid");
+    inputPhoneError.setAttribute("aria-hidden", false);
+    inputPhoneError.setAttribute("aria-invalid", true);
+  }
+
+  if (!placeField.value) {
+    const inputPlaceError = document.getElementById("input_placeError");
+    inputPlaceError.classList.add("visible");
+    placeField.classList.add("invalid");
+    inputPlaceError.setAttribute("aria-hidden", false);
+    inputPlaceError.setAttribute("aria-invalid", true);
+  }
+
+  
+  if (!vatField.value) {
+    const inputVatError = document.getElementById("input_vatError");
+    inputVatError.classList.add("visible");
+    vatField.classList.add("invalid");
+    inputVatError.setAttribute("aria-hidden", false);
+    inputVatError.setAttribute("aria-invalid", true);
+  }
+  if (!shippingField.value) {
+    const inputShippingError = document.getElementById("input_shippingError");
+    inputShippingError.classList.add("visible");
+    firstNameField.classList.add("invalid");
+    nameError.setAttribute("aria-hidden", false);
+    nameError.setAttribute("aria-invalid", true);
+  }
+  if (!placeField.value) {
+    const inputDescriptionError = document.getElementById("inputDescriptionError");
+    inputDescriptionError.classList.add("visible");
+    shippingField.classList.add("invalid");
+     inputDescriptionError.setAttribute("aria-hidden", false);
+     inputDescriptionError.setAttribute("aria-invalid", true);
+  }
+  if (!openingField.value) {
+    const inputOpeningError = document.getElementById("input_openError");
+    inputOpeningError.classList.add("visible");
+    openingField.classList.add("invalid");
+    inputOpeningError.setAttribute("aria-hidden", false);
+    inputOpeningError.setAttribute("aria-invalid", true);
+  }
+  if (!closingField.value) {
+    const inputClosingError = document.getElementById("input_closingError");
+    inputClosingError.classList.add("visible");
+    closingField.classList.add("invalid");
+    inputClosingError.setAttribute("aria-hidden", false);
+    inputClosingError.setAttribute("aria-invalid", true);
+  }
+  return valid;
+
+  if (!dayOffField.value) {
+    const inputClosingError = document.getElementById("input_dayOffError");
+    inputDayOffError.classList.add("visible");
+    dayOffField.classList.add("invalid");
+   inputDayOffError.setAttribute("aria-hidden", false);
+   inputDayOffError.setAttribute("aria-invalid", true);
+  }
+  return valid;
+
+  if (!imgField.value) {
+    const inputClosingError = document.getElementById("input_imgError");
+    inpuImgError.classList.add("visible");
+    imgField.classList.add("invalid");
+    inpuImgError.setAttribute("aria-hidden", false);
+    inpuImgError.setAttribute("aria-invalid", true);
+  }
+  return valid;
+
+ 
+}
+
+
--- resources/views/pages/restaurant/create.blade.php
+++ resources/views/pages/restaurant/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="d-flex justify-content-center py-5 my_bg">
-    <form class="form" method="POST" action="{{ route('restaurant.store') }}" enctype="multipart/form-data">
+    <form class="form" name="create" method="POST" action="{{ route('restaurant.store') }}" enctype="multipart/form-data">
         @csrf
         <div class="d-flex">
                 <img class="Logo_filter" src="https://cdn.iconscout.com/icon/free/png-256/deliveroo-3442893-2875354.png" alt="logo" width="50" height="50"> 
@@ -12,52 +12,84 @@
             <div>
                 <label for="name">Nome</label>
                 <div>
-                    <input type="text" name="name">
+                    <input required id="name" type="text" name="name" >
+
+                   <span role="alert" id="nameError" aria-hidden="true">
+                       Riempi il campo del nome del ristorante
+                    </span>
                 </div>
             
                 <label for="description">Descrizione</label>
                 <div>
-                    <input type="text" name="description">
+                    <input id="input_description" type="text" name="description">
+                    <span role="alert" id="inputDescriptionError" aria-hidden="true">
+                        Riempi il campo della descrizione
+                     </span>
+                    
                 </div>
 
                 <label for="place">Indirizzo</label>
                 <div>
-                    <input type="text" name="place">
+                    <input id=input_place type="text" name="place">
+                    <span role="alert" id="input_placeError" aria-hidden="true">
+                        Aggiungi l'indirizzo
+                     </span>
                 </div>    
 
                 <label for="phone_number">Recapito Telefonico</label>
                 <div>
-                    <input type="text" name="phone_number">
+                    <input id="input_phone" type="text" name="phone_number">
+                    <span role="alert" id="input_phoneError" aria-hidden="true">
+                        Aggiungi Recapito telefonico
+                     </span>
                 </div>   
 
                 <label for="vat">Piva</label>
                 <div>
-                    <input type="text" name="vat">
+                    <input id="input_vat" type="text" name="vat">
+                    <span role="alert" id="input_vatError" aria-hidden="true">
+                        Aggiungi Partita Iva
+                     </span>
                 </div>
 
                 <label for="shipping_cost">Spesa spedizione</label>
                 <div>
-                    <input type="number" step="0.01" min="1" max="5" name="shipping_cost">
+                    <input id="input_shipping" type="number" step="0.01" min="1" max="5" name="shipping_cost">
+                    <span role="alert" id="input_shippingError" aria-hidden="true">
+                        Aggiungi Spesa di spedizione
+                     </span>
                 </div>       
 
                 <label for="opening_time">Orario di Apertura</label>
                 <div>
-                    <input type="time" name="opening_time">
+                    <input id="input_opening" type="time" name="opening_time">
+                    <span role="alert" id="input_openError" aria-hidden="true">
+                        Aggiungi orario di Apertura
+                     </span>
                 </div>   
 
                 <label for="closing_time">Orario di Chiusura</label>
                 <div>
-                    <input type="time" name="closing_time">
+                    <input id="input_closing" type="time" name="closing_time">
+                    <span role="alert" id="input_closingError" aria-hidden="true">
+                        Aggiungi orario di Chiusura
+                     </span>
                 </div>
 
                 <label for="closing_day">Giorno di chiusura</label>
                 <div>
-                    <input type="text" name="closing_day">
+                    <input id="day_off" type="text" name="closing_day">
+                    <span role="alert" id="input_dayOffError" aria-hidden="true">
+                        Aggiungi giorno di Chiusura
+                     </span>
                 </div>
 
                 <label for="img">Inserisci un immagine</label><br><br>
                 <div>
-                    <input type="file" name="img">
+                    <input id="input_img" type="file" name="img">
+                    <span role="alert" id="input_imgError" aria-hidden="true">
+                        Aggiungi un immagine
+                     </span>
                 </div>
 
                 <br>
@@ -75,7 +107,8 @@
                 </ul>
             </div>
         </div>
-        <input type="submit" value="CREATE NEW RESTAURANT" class="text-center">
+        <input id="submit" type="submit"  value="CREATE NEW RESTAURANT" class="text-center">
+     
     </form>
 </section>
 @endsection
