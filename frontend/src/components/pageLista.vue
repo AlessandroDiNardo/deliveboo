b21629df82242bcb0dab68af78a4e5a11f7c28d2
furkanGit <furkancaliskan9801@gmail.com>
2023-03-13 17:18:37 +0100

    created pageLissta.vue

---
+++ frontend/src/components/pageLista.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "pageLista"
+}
+</script>
+
+
+<template>
+
+
+
+</template>
+
+
+<style lang="scss" scoped>
+@use '../assets/scss/general.scss' as *;
+@use '../assets/scss/partials/variables.scss' as *;
+
+
+
+
+</style>
