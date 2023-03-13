75086e407b36a72a4778eca783a8842f50ab0502
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-13 13:15:00 +0100

    import components into app vue

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,93 @@
+<script>
+
+export default {
+    data() {
+        return {
+            image: 'Logo-Deliveboo.png'
+        }
+    },
+}
+
+
+
+</script>
+
+<template>
+    <header>
+        <nav class="navbar  bg-transparent w-75 m-auto">
+            <div class="container-fluid">
+                <a class="navbar-brand bg-transparent" href="#">
+                    <img :src="image" alt="Logo" width="40" height="34" class="d-inline-block align-text-top Logo_filter">
+                    <span class="mx-2" id="font_logo">Deliveboo</span>
+                </a>
+                <div>
+                    <button class="  mx-2 btn_nav rounded-2 p-1">
+                        <a class=" text-decoration-none link_nav " href="#">
+                            Registrati
+                        </a>
+                    </button>
+                    <button class=" mx-2  btn_nav rounded-2 p-1">
+                    <a class="  text-decoration-none  link_nav  " href="#">
+                        Area Riservata
+                    </a>
+                </button>
+                    <button class=" mx-2  rounded-5 p-1 bg-white border-0">
+                        <a class="  text-decoration-none text-primary  link_nav  " href="#">
+                            <i class="bi bi-cart mx-1"></i>
+                        </a>
+                    </button>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- <section>
+                            <div class="video_container m-0 p-0 bg-black ">
+                                <video class="video_bg" src="../assets/video-jumbo.mp4" autoplay loop muted></video>
+                            </div>
+                        </section> -->
+</template>
+
+<style lang="scss">
+@use '../assets/scss/general.scss' as *;
+@use '../assets/scss/partials/variables.scss' as *;
+
+header {
+    background-color: $black;
+
+    .Logo_filter {
+        filter: invert(57%) sepia(70%) saturate(1956%) hue-rotate(108deg) brightness(91%) contrast(95%);
+    }
+
+
+    #font_logo {
+
+        font-weight: 900;
+        color: #069e24;
+
+
+    }
+
+    .btn_nav {
+        background-color: #069e24;
+        font-weight: 600;
+        transition: ease-in-out 1s;
+        border: none;
+
+        &:hover {
+            background-color: white;
+            border: #00eb56 1px solid;
+
+
+        }
+    }
+
+    .link_nav {
+        transition: ease-in-out 1s;
+        color: white;
+    }
+
+    .btn_nav .link_nav:hover {
+        color: #069e24;
+    }
+}
+</style>
---
+++ frontend/src/components/pageRestaurants.vue
@@ -0,0 +1,219 @@
+<script>
+export default {
+    name: "pageRestaurants"
+}
+</script>
+
+<template>
+    <main>
+        <!-- header main content -->
+        <div class=" border border-1 header_main_cont">
+            <section class=" ms_container d-flex justify-content-between align-items-center px-5 py-3">
+                <div class="side-bar">
+                    <button class="btn btn-color btn-primary" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">Filtri</button>
+
+                    <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions"
+                        aria-labelledby="offcanvasWithBothOptionsLabel">
+                        <div class="offcanvas-header">
+                            <div class="search_container d-flex justify-content-center align-items-center">
+                                <font-awesome-icon icon="fa-solid fa-magnifying-glass"
+                                    class="ms_search_icon border border-1 p-3" />
+                                <input type="text" placeholder="Search" class="ms_search_bar my-2 px-3">
+                            </div>
+                            <button type="button" class="btn btn-color-close btn-color" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class=" filter_cont d-flex flex-wrap justify-content-center align-items-center gap-4">
+                                <li>
+                                    Italiano
+                                    <input type="checkbox">
+                                </li>
+                                <li>
+                                    Cinese
+                                    <input type="checkbox">
+                                </li>
+                                <li>
+                                    Giapponese
+                                    <input type="checkbox">
+                                </li>
+                                <li>
+                                    Kebab
+                                    <input type="checkbox">
+                                </li>
+                                <li>
+                                    Hamburger
+                                    <input type="checkbox">
+                                </li>
+                                <li>
+                                    Sushi
+                                    <input type="checkbox">
+                                </li>
+                                <li>
+                                    Dolci
+                                    <input type="checkbox">
+                                </li>
+                                <li>
+                                    Messicano
+                                    <input type="checkbox">
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="d-flex justify-content-end align-items-end p-5">
+                            <button type="button" class="btn btn-color btn-outline-success">Filter</button>
+                        </div>
+                    </div>
+                </div>
+
+                <h3>Cerca il tuo ristorante, cibo o piatto preferito!</h3>
+            </section>
+        </div>
+
+        <!-- main content -->
+        <section>
+            <div class=" ms_container py-5 d-flex flex-wrap justify-content-center align-items-center gap-4">
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card ms_card">
+                    <img src="/public/img/git.png" class="card-img-top" alt="">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="btn btn-color btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/general.scss' as *;
+@use '../assets/scss/partials/variables.scss' as *;
+
+main {
+    background-color: $bg-main;
+}
+
+.ms_container,
+.log_section {
+    padding: 20px 0;
+}
+
+.ms_search_bar {
+    width: 250px;
+    height: 50px;
+    border: 1px solid rgb(0, 0, 0);
+}
+
+.ms_search_icon {
+    background-color: $bg-main;
+}
+
+.ms_card {
+
+    width: 400px;
+    height: 100%;
+}
+
+img {
+    height: 300px;
+    width: 100%;
+}
+
+.filter_cont {
+    width: 100%;
+    padding: 0px 30px;
+}
+
+ul {
+    list-style-type: none;
+}
+
+.btn-color {
+    background-color: $main-color;
+    border: none;
+}
+</style>
