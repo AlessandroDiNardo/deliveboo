b53b88397be9269007fe0c35baf3dcf1cc040461
luigicara <luigi.caravelli98@gmail.com>
2023-03-13 18:36:29 +0100

    added axios dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 
 // import bootstrap
 import "bootstrap/dist/css/bootstrap.min.css"
