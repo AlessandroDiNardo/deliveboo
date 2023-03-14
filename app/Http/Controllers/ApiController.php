210e1540898615b09ed511aec16aa6c3eff2323e
luigicara <luigi.caravelli98@gmail.com>
2023-03-14 09:31:17 +0100

    added comments to ApiController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -12,22 +12,31 @@ use App\Models\Product;
 class ApiController extends Controller
 {
     public function getRestaurants(Request $request) {
+        // checking categories in GET
         $categories = $request -> categories;
 
+        // saving all instances of Restaurant if no filter received as categories
         $restaurants = Restaurant::all();
 
+        // if there are...
         if ($categories) {
             $restaurants = null;
 
             foreach ($categories as $category) {
+                // if restaurants is empty
                 if (is_null($restaurants)) {
+
                     $restaurants = Restaurant::whereHas('categories', function ($q) use ($category) {
+                        
                         $q->where('category_id', $category);
+
                     })->get();
                 }
-
+                // if not...
                 $restaurants = $restaurants ->merge(Restaurant::whereHas('categories', function ($q) use ($category) {
+                    
                     $q->where('category_id', $category);
+                    
                 })->get());
             }
         }
