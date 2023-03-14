44334612ffae7d02dc2cc5d65bfbbbdb05bdad1b
Yari <narducciyarigmail.com>
2023-03-14 21:19:38 +0100

    risolto confltto valdiczione categories

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,7 +22,7 @@ function validate(e) {
   const closingField = document.getElementById("input_closing");
   const dayOffField = document.getElementById("day_off");
   const imgField = document.getElementById("input_img");
-  const categoryField = document.getElementById("input_category");
+  const categoryField = document.querySelectorAll(".category");
  
   let valid = true;
 
@@ -32,7 +32,7 @@ function validate(e) {
      firstNameField.classList.add("invalid");
      nameError.setAttribute("aria-hidden", false);
      nameError.setAttribute("aria-invalid", true);
-     nameError.innerHTML+= btnAlert;
+     
    }
   if (!descriptionField.value) {
     const inputDescriptionError = document.getElementById("inputDescriptionError");
@@ -66,15 +66,21 @@ function validate(e) {
     inputVatError.setAttribute("aria-hidden", false);
     inputVatError.setAttribute("aria-invalid", true);
   }
-  // if (isNaN(shippingField.value)) {
-  //   const inputShippingError = document.getElementById("input_shippingError");
-  //   inputShippingError.classList.add("visible");
-  //   firstNameField.classList.add("invalid");
-  //   nameError.setAttribute("aria-hidden", false);
-  //   nameError.setAttribute("aria-invalid", true);
-  // }
+  if (shippingField.value.trim()===0) {
+    // const inputShippingError = document.getElementById("input_shippingError");
+    // inputShippingError.classList.add("visible");
+    // shippingField.classList.add("invalid");
+    // inputShippingError.setAttribute("aria-hidden", false);
+    // inputShippingError.setAttribute("aria-invalid", true);
+    alert("Non Selezionata")
+  }
   else{
-    
+    // const inputShippingError = document.getElementById("input_shippingError");
+    // inputShippingError.classList.remove("visible");
+    // shippingField.classList.remove("invalid");
+    // inputShippingError.setAttribute("aria-hidden", true);
+    // inputShippingError.setAttribute("aria-invalid", false);
+    alert("Selezionata")
   }
   if (!placeField.value) {
     const inputDescriptionError = document.getElementById("inputDescriptionError");
@@ -115,19 +121,135 @@ function validate(e) {
     inputImgError.setAttribute("aria-hidden", false);
     inputImgError.setAttribute("aria-invalid", true);
   }
-  if (categoryField.checked) {
-    return valid;
-  }
-  else if(!categoryField.value){
-    const inputCategoryError = document.getElementById("input_categoryError");
-    inputCategoryError.classList.add("visible");
-    categoryField.classList.add("invalid");
-    inputCategoryError.setAttribute("aria-hidden", false);
-    inputCategoryError.setAttribute("aria-invalid", true);
-  }
+  let isChecked = false;
+    categoryField.forEach((category) => {
+      if (category.checked) {
+       isChecked = true;
+      
+      }
+    }
+     );
+  
+    if (!isChecked) {
+      const inputCategoryError = document.querySelector('#input_categoryError');
+      inputCategoryError.classList.add("visible");
+      inputCategoryError.setAttribute("aria-hidden", false);
+      inputCategoryError.setAttribute("aria-invalid", true);
+     } else {
+      const inputCategoryError = document.querySelector('#input_categoryError');
+      inputCategoryError.classList.remove("visible");
+      inputCategoryError.setAttribute("aria-hidden", true);
+      inputCategoryError.setAttribute("aria-invalid", false);
+     }
   return valid;
 
  
 }
+
+// const form = document.querySelector('.form');
+// const nameField = document.querySelector('#name');
+// const descriptionField = document.querySelector('#input_description');
+// const placeField = document.querySelector('#input_place');
+// const phoneField = document.querySelector('#input_phone');
+// const vatField = document.querySelector('#input_vat');
+// const shippingField = document.querySelector('#input_shipping');
+// const openingField = document.querySelector('#input_opening');
+// const closingField = document.querySelector('#input_closing');
+// const dayOffField = document.querySelector('#day_off');
+// const imgField = document.querySelector('#input_img');
+// const categoryField = document.querySelectorAll('.category');
+
+// form.addEventListener('submit', (event) => {
+//   // Stop form submission
+ 
+  
+//   // Validate name field
+//   if (nameField.value.trim() === '') {
+//     document.querySelector('#nameError').setAttribute('aria-hidden', false);
+    
+//   } else {
+//     document.querySelector('#nameError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate description field
+//   if (descriptionField.value.trim() === '') {
+//     document.querySelector('#inputDescriptionError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#inputDescriptionError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate place field
+//   if (placeField.value.trim() === '') {
+//     document.querySelector('#input_placeError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#input_placeError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate phone field
+//   if (phoneField.value.trim() === '') {
+//     document.querySelector('#input_phoneError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#input_phoneError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate VAT field
+//   if (vatField.value.trim() === '') {
+//     document.querySelector('#input_vatError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#input_vatError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate shipping field
+//   if (shippingField.value.trim() === '') {
+//     document.querySelector('#input_shippingError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#input_shippingError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate opening field
+//   if (openingField.value.trim() === '') {
+//     document.querySelector('#input_openError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#input_openError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate closing field
+//   if (closingField.value.trim() === '') {
+//     document.querySelector('#input_closingError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#input_closingError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate day off field
+//   if (dayOffField.value.trim() === '') {
+//     document.querySelector('#input_dayOffError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#input_dayOffError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate image field
+//   if (imgField.value.trim() === '') {
+//     document.querySelector('#input_imgError').setAttribute('aria-hidden', false);
+//   } else {
+//     document.querySelector('#input_imgError').setAttribute('aria-hidden', true);
+//   }
+
+//   // Validate category field
+//   let isChecked = false;
+//   categoryField.forEach((category) => {
+//     if (category.checked) {
+//       isChecked = true;
+//       alert("selezionato")
+//     }
+//   }
+//   );
+
+//   if (!isChecked) {
+//     document.querySelector('#input_categoryError').setAttribute('aria-hidden', false);
+//     alert("non selezionato")
+//   } else {
+//     document.querySelector('#input_categoryError').setAttribute('aria-hidden', true);
+//   }})
+
 
 
