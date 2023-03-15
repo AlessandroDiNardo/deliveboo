d4d133ddfff38a44ea24b45aeb3fec2fe9ba69cd
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-15 18:21:41 +0100

    change name from home to appMain

--- frontend/src/components/appMain.vue
+++ frontend/src/components/appMain.vue
@@ -1,9 +1,11 @@
 <script >
+import appVideo from './appVideo.vue';
 import pageRestaurants from './pageRestaurants.vue';
 import Section1 from './Section1.vue';
 
 export default {
     components: {
+        appVideo,
         pageRestaurants,
         Section1,
     },
@@ -12,7 +14,8 @@ export default {
 
 <template>
     <main>
+        <appVideo v-if="$route.name === 'home'" />
         <pageRestaurants />
-        <Section1 />
+        <Section1 v-if="$route.name === 'home'" />
     </main>
 </template>
