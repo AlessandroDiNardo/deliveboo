c0737123f600479b2e5b3c0e0fd2b7783f2d4d4f
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-15 11:12:06 +0100

    create base for single restaurant

---
+++ frontend/src/components/singleRestaurant.vue
@@ -0,0 +1,47 @@
+<script>
+import axios from 'axios';
+
+export default {
+
+    name: "singleRestautants",
+
+    data() {
+        return {
+
+            restaurants: [],
+            products: []
+        }
+    },
+    methods: {
+        getRestaurants() {
+            axios.get('http://localhost:8000/api/v1/restaurants/search', { params: { categories: this.filteredCategories } })
+                .then(res => {
+                    const data = res.data;
+                    const success = data.success;
+                    this.filteredRestaurants = data.response;
+                })
+                .catch(err => console.error(err));
+        },
+        getProducts() {
+            axios.get('http://localhost:8000/api/v1/products/all')
+                .then(res => {
+                    const data = res.data;
+                    const success = data.success;
+                    this.categories = data.response;
+                })
+                .catch(err => console.error(err));
+        },
+    },
+    mounted() {
+
+        this.getRestaurants();
+        this.getProducts()
+    },
+}
+</script>
+
+<template>
+    <h1>siuuuuuuuuu</h1>
+</template>
+
+<style></style>
