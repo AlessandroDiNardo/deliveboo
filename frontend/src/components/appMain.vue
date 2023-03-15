4d789cc1846e6b3fa277bbf418aa72f15d1db993
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-15 19:45:09 +0100

    create route for single restaurant

--- frontend/src/components/appMain.vue
+++ frontend/src/components/appMain.vue
@@ -1,12 +1,14 @@
 <script >
 import appVideo from './appVideo.vue';
 import pageRestaurants from './pageRestaurants.vue';
+import singleRestaurant from './singleRestaurant.vue';
 import Section1 from './Section1.vue';
 
 export default {
     components: {
         appVideo,
         pageRestaurants,
+        singleRestaurant,
         Section1,
     },
 }
@@ -15,7 +17,8 @@ export default {
 <template>
     <main>
         <appVideo v-if="$route.name === 'home'" />
-        <pageRestaurants />
+        <pageRestaurants v-if="$route.name === 'home'" />
+        <singleRestaurant v-if="$route.name === 'restaurant'" />
         <Section1 v-if="$route.name === 'home'" />
     </main>
 </template>
