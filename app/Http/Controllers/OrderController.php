6c9a3e24273dd2c3b77dbb4eeeb9779c83fed932
LucaVitale00 <luca16vitale2000@gmail.com>
2023-03-17 16:35:41 +0100

    create OrderController

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Order;
+use App\Models\Restaurant;
+
+class OrderController extends Controller
+{
+    public function show()
+    {
+
+    }
+}
+
