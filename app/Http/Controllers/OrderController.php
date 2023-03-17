42bcc279e8ad84e802556504a7db1ab73273e108
LucaVitale00 <luca16vitale2000@gmail.com>
2023-03-17 17:50:37 +0100

    request restaurant_id in OrderProduct

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,12 +6,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Order;
 use App\Models\Restaurant;
+use App\Models\Product;
 
 class OrderController extends Controller
 {
-    public function show()
-    {
+    public function show(Request $request) {
 
+        $restaurant = Restaurant::where('user_id', $request -> user() -> id) -> first();
+
+        $product = Product::all();
+
+        return view('pages.order.show', compact('restaurant', 'product'));
     }
 }
 
--- resources/views/pages/order/show.blade.php
+++ resources/views/pages/order/show.blade.php
@@ -2,5 +2,12 @@
 
 @section('content')
 
+ORDINI RICEVUTI:
+@foreach ($restaurant -> orders as $order)
+
+<h6 class="text-dark">ID: {{ $order -> id}}</h6>
+<h6 class="text-dark">PREZZO: {{ $order -> price}}</h6>
+ 
+@endforeach
 
 @endsection
