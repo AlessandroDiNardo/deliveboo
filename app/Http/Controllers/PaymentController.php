c4d79f268557039a7e2fcc455170931b41abbf83
luigicara <luigi.caravelli98@gmail.com>
2023-03-19 16:26:01 +0100

    edited transaction() to calculate total price of order

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers;
 use App\Services\BraintreeService;
 use Illuminate\Http\Request;
 
+// imported Models
+use App\Models\Restaurant;
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Order;
+use App\Models\Product;
+
 class PaymentController extends Controller
 {
     protected $braintree;
@@ -27,7 +34,34 @@ class PaymentController extends Controller
 
     public function transaction(Request $request)
     {
-        $nonceFromTheClient = $request -> payment_method_nonce;
+        // metodo di pagamento
+        $nonceFromTheClient = $request -> paymentInfo['payment_method_nonce'];
+
+        // salva orderInfo in $data e rimuovi productsIds
+        $data = $request -> orderInfo;
+        unset($data['productsIds']);
+        
+        // salva i productsIds in una variabile
+        $productsIds = $request -> orderInfo['productsIds'];
+        // crea una collection vuota di products
+        $products = collect();
+
+        // creo una variabile in cui calcolare il prezzo totale dell'ordine 
+        $orderPrice = 0;
+
+        // per ogni id in $productsIds aggiungi il corrispondente prodotto nella collection vuota
+        foreach ($productsIds as $id) {
+            $newProduct = Product::where('id', $id) -> first();
+
+            $orderPrice += $newProduct -> price;
+
+            $products -> push($newProduct);
+        }
+
+        $data['price'] = $orderPrice;
+
+        // crea un ordine prendendo i dati da $data e riempiendo quelli vuoti usando la factory
+        $order = Order::factory() -> make($data);
 
         $transaction = $this->braintree->generateTransaction()->sale([
             'amount' => '10.00',
@@ -36,7 +70,8 @@ class PaymentController extends Controller
 
         return response() -> json([
             'success' => true,
-            'response' => $transaction,
+            // DEBUG
+            'response' => $order,
         ]);
     }
 
