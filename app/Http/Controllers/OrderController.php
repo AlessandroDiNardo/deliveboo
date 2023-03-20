7804786f0532609430b7f2ef17b864e93144e3c0
LucaVitale00 <luca16vitale2000@gmail.com>
2023-03-20 17:03:25 +0100

    created function orderBy('order_date', 'desc')

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,11 +15,12 @@ class OrderController extends Controller
         $user = Auth::user();
         $restaurant_id = Restaurant::where('user_id', $user->id)->value('id');
 
-        // Recupera gli ordini associati al ristorante e i relativi dettagli dei prodotti
+        // Recupera gli ordini associati al ristorante e i relativi dettagli dei prodotti, ordinati per data in modo decrescente
         $orders = Order::where('restaurant_id', $restaurant_id)
                     ->with('products')
+                    ->orderBy('order_date', 'desc')
                     ->get();
 
         return view('pages.order.show', compact('orders'));
-}
+    }
 }
