6910c30cff912360411178461220657823e54278
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-20 11:49:22 +0100

    fix layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,15 @@
 <section>
 
     <h1 class="text-center py-5">
-        <span class="bold text-warning">Accedi</span> o <span class="bold text-info">Registrati</span> per usufrutire del nostro servizio!
+        Accedi o Registrati per usufrutire del nostro servizio!
     </h1>
         
         <div class="d-flex justify-content-center align-items-center gap-5">
-            <button class="btn btn-warning">
+            <button class="btn btn-success">
                 <a class="text-white text-decoration-none py-5 px-5" href="{{ route('login') }}">{{ __('Accedi') }}</a>
             <button>
 
-            <button class="btn btn-info">
+            <button class="btn btn-success">
                 <a class="text-white text-decoration-none py-3 px-5" href="{{ route('register') }}">{{ __('Registrati') }}</a>
             <button>
         </div>
