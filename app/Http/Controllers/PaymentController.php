d5441f173c59335ce04e1cc819296b797bae2b99
luigicara <luigi.caravelli98@gmail.com>
2023-03-20 11:06:27 +0100

    added orderStore() and edited transaction output

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 
 // imported Models
 use App\Models\Restaurant;
-use App\Models\Category;
 use App\Models\User;
 use App\Models\Order;
 use App\Models\Product;
@@ -43,11 +42,11 @@ class PaymentController extends Controller
         // salva i productsIds in una variabile
         $productsIds = $request -> paymentInfo['productsIds'];
 
-        // crea una collection vuota di products
-        // $products = collect();
-
         // creo una variabile in cui calcolare il prezzo totale dell'ordine 
         $orderPrice = 0;
+
+        // creo una variabile in cui salvare restaurant
+        $restaurant = null;
 
         // per ogni id in $productsIds aggiungi il corrispondente prodotto nella collection vuota
         foreach ($productsIds as $key => $id) {
@@ -57,32 +56,56 @@ class PaymentController extends Controller
             // aggiungi alla somma il prezzo del prodotto
             $orderPrice += $newProduct -> price;
 
-            // se il ciclo arriva all'ultimo prodotto
+            // quando il ciclo arriva all'ultimo prodotto...
             if ($key == count($productsIds) - 1) {
-                // aggiungi alla somma lo shipping_cost del ristorante da cui si acquista
-                $orderPrice += Restaurant::where('id', $newProduct -> restaurant_id) -> first() -> shipping_cost;
-            }
 
-            // $products -> push($newProduct);
+                // salva ristorante
+                $restaurant = Restaurant::where('id', $newProduct -> restaurant_id) -> first();
+
+                // aggiungi alla somma lo shipping_cost del ristorante da cui si acquista
+                $orderPrice +=  $restaurant -> shipping_cost;
+            }
         }
 
         // aggiungi la key price con l'intero prezzo all'array $data
         $data['price'] = $orderPrice;
 
-        // crea un ordine prendendo i dati da $data e riempiendo quelli vuoti usando la factory
-        $order = Order::factory() -> make($data);
-
+        // esegui la transazione con Braintree
         $transaction = $this->braintree->generateTransaction()->sale([
-            'amount' => '10.00',
+            'amount' => $orderPrice,
             'paymentMethodNonce' => $nonceFromTheClient,
         ]);
 
+        $order = null;
+
+        // se va a buon fine salva ordine
+        if ($transaction -> success) {
+            $order = $this -> orderStore($data, $productsIds, $restaurant);
+        }
+
         return response() -> json([
             'success' => true,
-            // DEBUG
-            'response' => $order,
+            'response' => [
+                'transaction' => $transaction,
+                'order' => $order
+            ],
         ]);
     }
+
+
+    public function orderStore($data, $productsIds, $restaurant) {
+
+        // crea un ordine prendendo i dati da $data e riempiendo quelli vuoti usando la factory
+        $order = Order::factory() -> make($data);
+
+        $order -> restaurant() -> associate($restaurant);
+
+        $order -> save();
+
+        $order -> products() -> attach($productsIds);
+
+        return $order;
+    } 
 
 
     // altre funzioni del controller per elaborare le transazioni di Braintree
