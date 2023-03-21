5c38d97f39abd8ff6a8adafae265b8d476230355
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-21 19:54:05 +0100

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,14 +57,14 @@
                             </li>
                             @endif
                             @else
-                            <li class="nav-item dropdown green">
+                            <li class="nav-item dropdown text-success">
                                 <a id="navbarDropdown" class="text-white nav-link ms_link dropdown-toggle"  href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item text-success" href="{{ url('profile') }}">{{__('Profile')}}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                    <a class="dropdown-item text-success" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
