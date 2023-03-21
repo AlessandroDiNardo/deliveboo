5818a6fa998529d7d36fbbc9814b56d9e7b64d31
luigicara <luigi.caravelli98@gmail.com>
2023-03-21 15:30:42 +0100

    mail classes

---
+++ app/Mail/NewOrderRestaurant.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewOrderRestaurant extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $order;
+    
+    public function __construct($order)
+    {
+        $this -> order = $order;
+    }
+
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'New Order N°' . $this -> order -> code,
+            replyTo: env('MAIL_FROM_ADDRESS', 'hello@example.com')
+        );
+    }
+
+    public function content()
+    {
+        return new Content(
+            view: 'mail.new-order-restaurant',
+        );
+    }
+}
