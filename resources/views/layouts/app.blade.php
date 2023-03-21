a322b767148fe9fa7d9cd4fabb678dd2c5baa321
AlessandroDiNardo <alexdna003@gmail.com>
2023-03-21 22:43:59 +0100

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('/ms-icon-70x70.png') }}" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
