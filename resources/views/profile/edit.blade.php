1ecb24af8e84e616d958b12aa61ac96eb85efa70
luigicara <luigi.caravelli98@gmail.com>
2023-03-21 16:25:59 +0100

    added modal to confrim delete

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -42,9 +42,41 @@
                     Modifica Ristorante
                 </a>
 
-                <a href="{{ route('restaurant.delete', $restaurant) }}" class="btn btn-danger w-25">
+                <button type="button" class="btn btn-danger w-25" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     Elimina Ristorante
-                </a>
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+
+                        <div class="modal-content">
+                            <div class="modal-header">
+
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Cancellazione ristorante!</h1>
+
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+                            </div>
+
+                            <div class="modal-body">
+                                Questa azione cancellerà per sempre il tuo ristorante e i suoi dati, compresi tutti i prodotti e lo storico ordini.
+                            </div>
+
+                            <div class="modal-footer">
+
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+                                <a href="{{ route('restaurant.delete', $restaurant) }}" class="text-reset text-decoration-none">
+                                    <button type="button" class="btn btn-danger">
+                                        Elimina
+                                    </button>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
 
                 <a href="{{ route('product.showAll') }}" class="btn btn-success w-25">
                     Vai ai Prodotti
